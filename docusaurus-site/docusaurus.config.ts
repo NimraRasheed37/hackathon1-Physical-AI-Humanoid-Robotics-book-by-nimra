d1330ba8b35ace7e65ed0ae2f2ee1270722fd5cb
Nimra Rasheed <60661441+NimraRasheed37@users.noreply.github.com>
2025-12-25 00:54:47 +0500

    Update docusaurus.config.ts

--- docusaurus-site/docusaurus.config.ts
+++ docusaurus-site/docusaurus.config.ts
@@ -15,14 +15,14 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://nimra-mehmood.github.io',
+  url: 'https://hackathon1-physical-ai-humanoid-rob-delta.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'nimra-mehmood', // Usually your GitHub org/user name.
+  organizationName: 'nimra-rasheed', // Usually your GitHub org/user name.
   projectName: 'AI-textbook', // Usually your repo name.
 
   onBrokenLinks: 'throw',
